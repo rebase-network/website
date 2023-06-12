cefb84b744b166df4bbbf29e036e6c2916842ec5
liushooter <shooter.hero@gmail.com>
2023-06-12 21:48:05 +0800

    up api config

--- packages/backend/config/api.js
+++ packages/backend/config/api.js
@@ -1,7 +1,11 @@
-module.exports = {
+module.exports = ({ env }) => ({
+  responses: {
+    privateAttributes: ['_v', 'id', 'createdAt', 'updatedAt'],
+  },
   rest: {
+    prefix: '/api/v1',
     defaultLimit: 25,
     maxLimit: 100,
     withCount: true,
   },
-};
+});
