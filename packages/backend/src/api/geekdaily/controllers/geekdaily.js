9ffb6c35ad4e5b40e74f6c7fc152d04849aed62e
liushooter <shooter.hero@gmail.com>
2023-06-20 12:10:59 +0800

    batch Insert

--- packages/backend/src/api/geekdaily/controllers/geekdaily.js
+++ packages/backend/src/api/geekdaily/controllers/geekdaily.js
@@ -3,7 +3,25 @@
 /**
  * geekdaily controller
  */
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::geekdaily.geekdaily');
+module.exports = createCoreController('api::geekdaily.geekdaily', ({ strapi }) => ({
+  async batchInsert(ctx) {
+    const dataToInsert = ctx.request.body;
+
+    try {
+      const result = await strapi.db.query("api::geekdaily.geekdaily").createMany({
+        data: dataToInsert.data,
+      });
+
+      // return {"count":3,"ids":[6,7,8]}
+      ctx.send(result);
+
+    } catch (error) {
+      console.error(error);
+      ctx.throw(500, 'Internal Server Error');
+    }
+
+  }
+
+}));
--- packages/backend/src/api/geekdaily/routes/geekdaily.js
+++ packages/backend/src/api/geekdaily/routes/geekdaily.js
@@ -6,4 +6,31 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::geekdaily.geekdaily');
+const defaultRouter = createCoreRouter("api::geekdaily.geekdaily");
+
+const customRouter = (innerRouter, extraRoutes = []) => {
+  // https://forum.strapi.io/t/how-to-add-custom-routes-to-core-routes-in-strapi-4/14070/7
+
+  let routes;
+
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes.concat(extraRoutes);
+      return routes;
+    },
+  };
+};
+
+
+const myExtraRoutes = [
+  {
+    method: "POST",
+    path: "/geekdailies/batchInsert",
+    handler: "api::geekdaily.geekdaily.batchInsert",
+  }
+];
+
+module.exports = customRouter(defaultRouter, myExtraRoutes);
